7c72e084e1e0b8830ccf296aad6db9ef967b8b3d
kciceblue <kciceblue@outlook.com>
2024-06-20 15:19:59 +0100

    Continued

--- bioReactionProtocol.py
+++ bioReactionProtocol.py
@@ -3,23 +3,29 @@ from opentrons import protocol_api as op_api
 requirements = {"robot type": "Flex", "apiLevel": "2.18"}  # set according to examples
 
 # labware settings, fill accordingly
-tipType = "opentrons_flex_96_tiprack_1000ul"
-tipLoc = "C4"
-pipetteSingular = "flex_1channel_1000"
-pipette8Channels = "flex_8channel_1000"
+tipType1 = "opentrons_flex_96_tiprack_1000ul"
+tipLoc1 = "C4"
+tipType2 = "opentrons_flex_96_tiprack_50ul"
+tipLoc2 = "B4"
 
 
 def run(protocol: op_api.ProtocolContext):
-    # load tip rack
-    tipRack = protocol.load_labware(
-        load_name=tipType,
-        location=tipLoc
+    # load tip rack1
+    tipRack1 = protocol.load_labware(
+        load_name=tipType1,
+        location=tipLoc1
+    )
+
+    # load tip rack2
+    tipRack2 = protocol.load_labware(
+        load_name=tipType2,
+        location=tipLoc2
     )
 
     # attach tips to selected pipette
-    pipette = protocol.load_instrument(
-        instrument_name=pipetteSingular,
+    pipetteL = protocol.load_instrument(
+        instrument_name="flex_1channel_1000",
         mount="left",
-        tip_racks=[tipRack]
+        tip_racks=[tipRack1]
     )
 
