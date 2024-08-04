2b3c406afcca05b44473d34178d3738a005ac5d2
kciceblue <kciceblue@outlook.com>
2024-08-04 13:27:14 +0100

    Continued

--- bioReactionProtocol.py
+++ bioReactionProtocol.py
@@ -1,12 +1,22 @@
 from opentrons import protocol_api as op_api
 
-requirements = {"robot type": "Flex", "apiLevel": "2.18"}  # set according to examples
+# metadata
+metadata = {
+    "protocolName": "TX014",
+    "author": "Tom Xu",
+    "description": "Protocol for biocatalytic reactions to evaluate enzymes in TX014",
+}
+
+requirements = {"robot type": "Flex", "apiLevel": "2.19"}  # set according to examples
 
 # labware settings, fill accordingly
 tipType1 = "opentrons_flex_96_tiprack_1000ul"
 tipLoc1 = "C4"
-tipType2 = "opentrons_flex_96_tiprack_50ul"
+tipType2 = "opentrons_flex_96_tiprack_200ul"
 tipLoc2 = "B4"
+hsModName = "heaterShakerModuleV1"
+hsAdapter = "opentrons_universal_flat_adapter"
+hsPlate = "nest_96_wellplate_2ml_deep"
 
 
 def run(protocol: op_api.ProtocolContext):
@@ -28,4 +38,24 @@ def run(protocol: op_api.ProtocolContext):
         mount="left",
         tip_racks=[tipRack1]
     )
+
+    # attach tips to selected pipette
+    pipetteS = protocol.load_instrument(
+        instrument_name="flex_1channel_1000",
+        mount="left",
+        tip_racks=[tipRack2]
+    )
+
+    # load trash
+    trash = protocol.load_trash_bin(location="A3")
+
+    # load heater shaker mod
+    hs_mod = protocol.load_module(
+        module_name=hsModName, location="C1"
+    )
+
+    # load heater shaker adapter
+    hs_adapter = hs_mod.load_adapter(hsAdapter)
+    # load heater shaker plate
+    hs_plate = hs_adapter.load_labware(hsPlate)
 
